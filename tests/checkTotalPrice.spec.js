063c86b981ef587cbcc9c5a5693f7e45b5c9ff98
vlada022 <vladimir_perisic@ymail.com>
2024-07-09 11:52:58 +0200

    ispravka 1

--- tests/checkTotalPrice.spec.js
+++ tests/checkTotalPrice.spec.js
@@ -5,7 +5,7 @@ import { CartPage } from '../pages/cartpage';
 import { CheckOutPageOne } from '../pages/checkoutPageOne';
 import { CheckOutPageTwo } from '../pages/checkoutPageTwo';
 
-test.only('Verify correct sum for all products in cart (TASK 5)', async ({ page }) => {
+test('Verify correct sum for all products in cart (TASK 5)', async ({ page }) => {
 
   //Login
   const Login = new LoginPage(page);
@@ -38,18 +38,18 @@ test.only('Verify correct sum for all products in cart (TASK 5)', async ({ page 
   const arrayAllPrices = await page.locator("div[class='cart_list'] > [class='cart_item'] >[class='cart_item_label'] [class='item_pricebar']").allTextContents();
 
   //Initialize array to push newly created float prices in for loop
-var arr = new Array();
-//Starting value of summarize 
-var sumAllProducts = 0;
-for (let i = 0; i < arrayAllPrices.length; i++) {
-    //Iterate trough all prices with indexing
-  var allPricesIterator = arrayAllPrices[i];
-    //Remove $ from Prices and parse it to Float number (with decimals)
-  arr.push(parseFloat(allPricesIterator.replace("$", "")));
-// Summarize all product prices
-  sumAllProducts = sumAllProducts + arr[i];
-}
-// Assertation Checking if summarize is matching Total Item Cost
-await expect(itemTotalFloat).toBe(sumAllProducts);
+  var arr = new Array();
+  //Starting value of summarize 
+  var sumAllProducts = 0;
+  for (let i = 0; i < arrayAllPrices.length; i++) {
+      //Iterate trough all prices with indexing
+    var allPricesIterator = arrayAllPrices[i];
+      //Remove $ from Prices and parse it to Float number (with decimals)
+    arr.push(parseFloat(allPricesIterator.replace("$", "")));
+  // Summarize all product prices
+    sumAllProducts = sumAllProducts + arr[i];
+  }
+  // Assertation Checking if summarize is matching Total Item Cost
+  await expect(itemTotalFloat).toBe(sumAllProducts);
   
 });
